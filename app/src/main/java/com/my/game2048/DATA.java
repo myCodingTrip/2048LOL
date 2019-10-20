94fd470dab46939c60972a02e1fa102f55a33ef6
MY <441015782@qq.com>
2019-10-20 18:16:06 +0800

    reformat

--- app/src/main/java/com/my/game2048/DATA.java
+++ app/src/main/java/com/my/game2048/DATA.java
@@ -1,15 +1,16 @@
 package com.my.game2048;
 
-import java.util.List;
-
 import android.graphics.Point;
+
+import java.util.List;
 
 public class DATA {
 
-	public static final int LINES = 4;
-	//CARD_WIDTH为小格子的边长，在onSizeChanged方法中赋值。
-	public static int CARD_WIDTH = 0;
-	public static Card[][] cardsMap = new Card[4][4];;
-	public static List<Point> emptyPoints;
-	public static boolean win;
+    public static final int LINES = 4;
+    //CARD_WIDTH为小格子的边长，在onSizeChanged方法中赋值。
+    public static int CARD_WIDTH = 0;
+    public static Card[][] cardsMap = new Card[4][4];
+    ;
+    public static List<Point> emptyPoints;
+    public static boolean win;
 }
--- app/src/main/java/com/my/game2048/MainActivity.java
+++ app/src/main/java/com/my/game2048/MainActivity.java
@@ -1,8 +1,6 @@
 package com.my.game2048;
 
 
-
-
 import android.app.Activity;
 import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
@@ -11,10 +9,12 @@ import android.view.MenuItem;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
-	GameView gameView;
-	public MainActivity() {
-		mainactivity = this;
-	}
+    GameView gameView;
+
+    public MainActivity() {
+        mainactivity = this;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,62 +23,71 @@ public class MainActivity extends Activity {
         gameView = (GameView) findViewById(R.id.gameView);
         animLayer = (AnimLayer) findViewById(R.id.animLayer);
         tvBestScore = (TextView) findViewById(R.id.tvBestScore);
-        
+
     }
+
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {        
+    public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+
     @Override
-    public boolean onOptionsItemSelected(MenuItem item){
-    	int id = item.getItemId();
-        if(id == R.id.start){
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.start) {
             gameView.startGame();
-            return  true;
+            return true;
         }
         return super.onOptionsItemSelected(item);
     }
-    public void clearScore(){
-    	score = 0;
-     	showScore();
+
+    public void clearScore() {
+        score = 0;
+        showScore();
     }
-    public void showScore(){
-    	tvScore.setText(score+"");
+
+    public void showScore() {
+        tvScore.setText(score + "");
     }
-    public void addScore(int s){
-    	score+=s;
-    	showScore();
-    	int maxScore = Math.max(score, getBestScore());
-		saveBestScore(maxScore);
-		showBestScore(maxScore);
+
+    public void addScore(int s) {
+        score += s;
+        showScore();
+        int maxScore = Math.max(score, getBestScore());
+        saveBestScore(maxScore);
+        showBestScore(maxScore);
     }
+
     //利用SharedPreferences长期存储bestScore
-    public void saveBestScore(int s){
-		Editor e = getPreferences(MODE_PRIVATE).edit();
-		e.putInt(SP_KEY_BEST_SCORE, s);
-		e.commit();
-	}
+    public void saveBestScore(int s) {
+        Editor e = getPreferences(MODE_PRIVATE).edit();
+        e.putInt(SP_KEY_BEST_SCORE, s);
+        e.commit();
+    }
 
-	public int getBestScore(){
-		return getPreferences(MODE_PRIVATE).getInt(SP_KEY_BEST_SCORE, 0);
-	}
+    public int getBestScore() {
+        return getPreferences(MODE_PRIVATE).getInt(SP_KEY_BEST_SCORE, 0);
+    }
 
-	public void showBestScore(int s){
-		tvBestScore.setText(s+"");
-	}
+    public void showBestScore(int s) {
+        tvBestScore.setText(s + "");
+    }
+
     public AnimLayer getAnimLayer() {
-		return animLayer;
-	}
+        return animLayer;
+    }
+
     private int score = 0;
     private TextView tvScore;
     private TextView tvBestScore;
     private static MainActivity mainactivity = null;
     private AnimLayer animLayer = null;
-    
+
     public static MainActivity getMainActivity() {
-		return mainactivity;
-	}
+        return mainactivity;
+    }
+
     public static final String SP_KEY_BEST_SCORE = "bestScore";
 }
